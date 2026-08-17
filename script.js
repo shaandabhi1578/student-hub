74e5e3c171b64ab441b24beb9bff7d4b92005e54
shaan <shaandabhi@gmail.com>
2026-08-17 14:05:39 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function showsum()
+{
+    let x=5;
+    let y=25;
+    document.getElementById("result").innerHTML=x+y;
+}
+
+<button type="button" onclick="showsum()">cllick me</button>
+
+  str=5;
+            str++;
+            printthook();
+            function printthook(){
+                document.write('thoook');
+            }
+             document.write(' shaan dabhi')
